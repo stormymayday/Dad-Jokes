22cfc35da33864c855f779aa6d3c13668d5d4555
stormymayday <stormymayday7@gmail.com>
2023-05-27 11:38:26 -0700

    Selecting the button

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import getElement from './utils/getElement.js';
 
-impport getElement
-
 const url = 'https://icanhazdadjoke.com/';
 
-console.log(url);
+// Element Selections
+const btn = getElement('.btn');
+console.log(btn);
