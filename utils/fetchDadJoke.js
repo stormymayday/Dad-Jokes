2b5e2bd28410ab6774eb7be0fec42f2c88275e6d
stormymayday <stormymayday7@gmail.com>
2023-05-28 07:20:37 -0700

    Setting up the fetchDadJoke function

---
+++ utils/fetchDadJoke.js
@@ -0,0 +1,14 @@
+const fetchDadJoke = async () => {
+
+    const response = await (fetch(url, {
+        headers: {
+            Accept: 'application/json',
+            'User-Agent': 'learning app'
+        },
+    }));
+
+    console.log(response);
+
+};
+
+export default fetchDadJoke;
